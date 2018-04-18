32450a0649f6e8f3a380b57056161de146a63c7f
Chris Brosnan <cbrosnan00@gmail.com>
2018-04-18 13:46:09 +0100

    first commit
    first commit

---
+++ cb-wp-post-stats.php
@@ -0,0 +1,14 @@
+<?php
+   /*
+   Plugin Name: CB-WP Post stats plugin
+   Plugin URI: 
+   Description: A small plugin for viewing stats on pages and posts
+   Version: 1.0
+   Author: Chris Brosnan
+   Author URI: http://github.com/chrisbrosnan/
+   License:
+   */
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,7 @@
+PREMIUM FEATURES: 
+- Export as CSV/JSON
+- Send to Email
+- GA stats
+- Support for custom post types
+- Support for custom taxonomies
+- Author stats
